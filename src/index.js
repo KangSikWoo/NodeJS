7893017be05f4b083294929aa90fd78cbe6f1102
Wookangsik <wks09254930@gmail.com>
2025-04-09 18:36:33 +0900

    env Configuration Settings

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
-// const express = require('express')  // -> CommonJS
-import express from 'express'; // -> ES Module
-import open from 'open'; // ESM일 경우
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
+
+app.use(cors()); // cors 방식 허용
+app.use(express.static('public')); // 정적 파일 접근
+app.use(express.json()); // request의 본문을 json으로 해석할 수 있도록 함 (JSON 형태의 요청 body를 파싱하기 위함)
+app.use(express.urlencoded({ extended: false })); // 단순 객체 문자열 형태로 본문 데이터 해석
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -12,5 +19,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
-open('http://localhost:3000');
